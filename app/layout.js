7a232bf20b84e1825f5ff0b08cacb01281a571ed
svenaci <steffan_v@hotmail.com>
2024-09-04 12:25:56 -0400

    add sign-in and sign-up page and enable google one tap

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, GoogleOneTap } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <GoogleOneTap />
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
