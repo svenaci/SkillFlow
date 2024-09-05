c5be50772e75e9c1fa6cf0768aa78e9ffac5f671
svenaci <steffan_v@hotmail.com>
2024-09-05 16:51:47 -0400

    create component to add topic description

--- app/create-course/_components/TopicDescription.jsx
+++ app/create-course/_components/TopicDescription.jsx
@@ -1,7 +1,30 @@
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import React from "react";
 
 function TopicDescription() {
-  return <div>TopicDescription</div>;
+  return (
+    <div className="mx-20 lg:mx-44">
+      <div className="mt-5">
+        <label>
+          Write the topic for which you want to generate a course (e.g. Java
+          Course, Yoga, etc.):
+        </label>
+        <Input placeholder={"Topic of your choice"} />
+      </div>
+      <div className="mt-5">
+        <label>
+          Tell us more about your course, what you want to include in the course
+          (Optional)
+        </label>
+        <Textarea
+          placeholder={
+            "Add some description about the course you want to generate"
+          }
+        />
+      </div>
+    </div>
+  );
 }
 
 export default TopicDescription;
