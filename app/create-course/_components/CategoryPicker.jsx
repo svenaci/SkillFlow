fee7524f9ff6cb82cc6f1b2615e90a4f113f4bc0
svenaci <steffan_v@hotmail.com>
2024-09-05 18:24:31 -0400

    add minor styling adjustements

--- app/create-course/_components/CategoryPicker.jsx
+++ app/create-course/_components/CategoryPicker.jsx
@@ -4,13 +4,16 @@ import React from "react";
 
 function CategoryPicker() {
   return (
-    <div className="grid grid-cols-3 gap-10 px-10 md:px-20">
-      {CategoryList.map((item, index) => (
-        <div className="flex flex-col p-5 border items-center rounded-xl hover:border-primary hover:bg-blue-50 cursor-pointer">
-          <Image src={item.icon} width={100} height={100} />
-          <h2>{item.name}</h2>
-        </div>
-      ))}
+    <div className=" px-10 md:px-20">
+      <h2 className="my-5">Select the course category</h2>
+      <div className="grid grid-cols-3 gap-10">
+        {CategoryList.map((item, index) => (
+          <div className="flex flex-col p-5 border items-center rounded-xl hover:border-primary hover:bg-blue-50 cursor-pointer">
+            <Image src={item.icon} width={100} height={100} />
+            <h2>{item.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- app/create-course/_components/TopicDescription.jsx
+++ app/create-course/_components/TopicDescription.jsx
@@ -6,14 +6,16 @@ function TopicDescription() {
   return (
     <div className="mx-20 lg:mx-44">
       <div className="mt-5">
-        <label>
+        💡
+        <label className="ml-2">
           Write the topic for which you want to generate a course (e.g. Java
           Course, Yoga, etc.):
         </label>
         <Input placeholder={"Topic of your choice"} />
       </div>
       <div className="mt-5">
-        <label>
+        📝
+        <label className="ml-2">
           Tell us more about your course, what you want to include in the course
           (Optional)
         </label>
