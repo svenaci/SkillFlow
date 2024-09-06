060bcce6533a6b5dfa6b8a1c581beb8800f46405
svenaci <steffan_v@hotmail.com>
2024-09-06 15:31:39 -0400

    add drizzle config to update or push our db changes

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./configs/schema.jsx",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING,
+  },
+};
