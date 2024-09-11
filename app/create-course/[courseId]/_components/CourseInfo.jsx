7ff6d35f8957f27c9680dc1ffac0a7c8181e1232
svenaci <steffan_v@hotmail.com>
2024-09-10 21:33:46 -0400

    allow user to upload a cover image for their course

--- app/create-course/[courseId]/_components/CourseInfo.jsx
+++ app/create-course/[courseId]/_components/CourseInfo.jsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { HiOutlinePuzzlePiece } from "react-icons/hi2";
 import EditCourse from "./EditCourse";
 
 export default function CourseInfo({ course, refreshData }) {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const onFileSelected = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(URL.createObjectURL(file));
+  };
+
   return (
     <div className="p-10 border rounded-xl shadow-sm mt-5">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
@@ -22,14 +29,22 @@ export default function CourseInfo({ course, refreshData }) {
           </h2>
           <Button className="w-full mt-5">Start</Button>
         </div>
-        {/* <div> */}
-        {/* <Image
-          src={"/book.png"}
-          width={300}
-          height={300}
-          className=" w-[200px] h-[200px] object-cover"
-        /> */}
-        {/* </div> */}
+        <div>
+          <label htmlFor="upload-image">
+            <Image
+              src={selectedFile ? selectedFile : "/book.png"}
+              width={300}
+              height={300}
+              className=" w-[200px] h-[200px] object-cover cursor-pointer"
+            />
+          </label>
+          <input
+            type="file"
+            id="upload-image"
+            className="opacity-0"
+            onChange={onFileSelected}
+          />
+        </div>
       </div>
     </div>
   );
