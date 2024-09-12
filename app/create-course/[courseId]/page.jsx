da89358e82e2b64ca6173eb466dbc171bbe6581a
svenaci <steffan_v@hotmail.com>
2024-09-11 23:48:04 -0400

    add button to generate chapter content and promp for our AI" " ;

--- app/create-course/[courseId]/page.jsx
+++ app/create-course/[courseId]/page.jsx
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from "react";
 import CourseInfo from "./_components/CourseInfo";
 import CourseDetail from "./_components/CourseDetail";
 import ChapterDetails from "./_components/ChapterDetails";
+import { Button } from "@/components/ui/button";
 
 function CoursePage({ params }) {
   const { user } = useUser();
@@ -29,12 +30,25 @@ function CoursePage({ params }) {
     setCourse(response[0]);
     console.log(response);
   };
+
+  const generateChapterContent = () => {
+    const chapters = course?.courseOutput?.chapters;
+    chapters.forEach((chapter, index) => {
+      const PROMPT =
+        "Explain the concept in Detail on Topic: " +
+        course?.name +
+        ", Chapter:" +
+        chapter?.name +
+        " in JSON format with field as title, description in detail, Code Example (HTML Code format) if applicable";
+    });
+  };
   return (
     <div className="mt-10 px-7 md:px-20 lg:px-44">
       <h2 className="font-bold text-center text-2xl">Course Layout</h2>
       <CourseInfo course={course} refreshData={() => getCourse()} />
       <CourseDetail course={course} />
       <ChapterDetails course={course} refreshData={() => getCourse()} />
+      <Button className="my-10">Generate Chapter Content</Button>
     </div>
   );
 }
